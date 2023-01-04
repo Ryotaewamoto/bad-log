527e7c64dcfc1a15660ab5da8c4d1a5d15077b22
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-04 15:17:46 +0900

    feat: set up Firebase
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

---
+++ lib/utils/settings.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options/firebase_options_dev.dart' as dev_options;
+import 'firebase_options/firebase_options_prod.dart' as prod_options;
+
+const flavorString = String.fromEnvironment('FLAVOR');
+
+/// dart-define で設定した flavor 文字列から特定される Flavor 変数。
+final flavor = Flavor.values.firstWhere((f) => f.name == flavorString);
+
+/// Flutter のビルドオプションの flavor
+enum Flavor {
+  local,
+  dev,
+  prod;
+
+  const Flavor();
+
+  /// 接続先 Firebase プロジェクト。
+  FirebaseOptions get firebaseOptions {
+    switch (this) {
+      case prod:
+        return prod_options.DefaultFirebaseOptions.currentPlatform;
+      case dev:
+      case local:
+        return dev_options.DefaultFirebaseOptions.currentPlatform;
+    }
+  }
+}
