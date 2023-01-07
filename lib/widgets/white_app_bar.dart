a2bfe7951b0e4d35fd2b7c759c3cab2f7b5626db
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-07 19:52:01 +0900

    feat: create white app bar widget
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

---
+++ lib/widgets/white_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../utils/constants/app_colors.dart';
+import '../utils/text_styles.dart';
+
+class WhiteAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const WhiteAppBar({
+    super.key,
+    required this.title,
+    this.leading,
+    this.actions,
+  });
+
+  final String title;
+  final Widget? leading;
+  final List<Widget>? actions;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      // TODO: バックボタンを SVG で表示
+      leading: leading,
+      centerTitle: true,
+      elevation: 0,
+      title: Text(title, style: TextStyles.h2()),
+      backgroundColor: AppColors.baseWhite,
+      iconTheme: const IconThemeData(color: AppColors.primary),
+      actions: actions,
+    );
+  }
+}
