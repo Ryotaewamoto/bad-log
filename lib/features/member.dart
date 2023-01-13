7ac86dc8a7ee37e04f8b5b32fdb2391c1fa27bea
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-13 16:27:01 +0900

    feat: fix flutter format
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- lib/features/member.dart
+++ lib/features/member.dart
@@ -4,7 +4,6 @@ import '../models/member.dart';
 import '../repositories/auth/auth_repository_impl.dart';
 import '../repositories/member/member_repository_impl.dart';
 
-
 /// ユーザーの attendingChatRoom コレクションを購読する StreamProvider。
 /// ユーザーがログインしていない場合は例外をスローする。
 final membersProvider = StreamProvider.autoDispose<List<Member>>((ref) {
