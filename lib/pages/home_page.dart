dfb8b1d99a12367ac2890639ec25c6ac3f219eb5
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2023-01-15 00:06:46 +0900

    feat: add MatchResultCard class for home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:bad_log/utils/constants/measure.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
 
 import '../utils/constants/app_colors.dart';
 import '../widgets/white_app_bar.dart';
@@ -54,6 +57,95 @@ class HomePage extends StatelessWidget {
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class MatchResultCard extends StatelessWidget {
+  const MatchResultCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.baseLight,
+      child: InkWell(
+        onTap: () {
+          // ここにタップ時の処理を記述
+          print('Card Tapped');
+        },
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: Measure.p_a8,
+            child: Column(
+              children: [
+                Container(
+                  // 対戦メンバーのコンテナー
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.topLeft,
+                      end: FractionalOffset.bottomRight,
+                      colors: [
+                        AppColors.baseWhite,
+                        AppColors.accent,
+                      ],
+                      stops: [
+                        0.5,
+                        1.0,
+                      ],
+                    ),
+                  ),
+                  child: Padding(
+                    padding: Measure.p_a8,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(Icons.circle,
+                                size: 20, color: AppColors.accent),
+                            Measure.g_8,
+                            Text('Jonatan Christie'),
+                          ],
+                        ),
+                        // ******* Gap(2)を定数化する *******
+                        const Gap(2),
+                        Row(
+                          children: const [
+                            Icon(Icons.circle,
+                                size: 20, color: AppColors.accent),
+                            Measure.g_8,
+                            Text('Thomas Rouxel'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: Measure.p_v4,
+                  child: Padding(
+                    padding: Measure.p_h8,
+                    child: Row(
+                      children: [
+                        // ******* 色がおかしい *******
+                        const Icon(Icons.watch_later,
+                            size: 18, color: AppColors.textColor),
+                        Measure.g_8,
+                        Text(DateFormat('yyyy/MM/dd').format(DateTime.now())),
+                      ],
+                    ),
+                  ),
+                ),
+                const Divider(
+                  height: 1,
+                  color: AppColors.textColor,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
