61b9638f32314708ae36214e8db1fb9d1a27d3f1
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-25 23:26:53 +0900

    feat: refactor result page
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../features/app_user.dart';
+import '../features/member.dart';
+import '../models/member.dart';
+import '../models/result.dart';
 import '../utils/constants/app_colors.dart';
 import '../utils/constants/measure.dart';
 import '../utils/extensions/date_time.dart';
+import '../utils/text_styles.dart';
 import '../widgets/white_app_bar.dart';
 
-class ResultPage extends StatelessWidget {
-  const ResultPage({super.key});
+class ResultPage extends HookConsumerWidget {
+  const ResultPage({
+    required this.result,
+    super.key,
+  });
+
+  final Result result;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appUserName = ref.watch(appUserFutureProvider).maybeWhen<String?>(
+          data: (data) => data?.userName,
+          orElse: () => null,
+        );
+    final members = ref.watch(membersProvider).maybeWhen<List<Member>>(
+          data: (data) {
+            return data;
+          },
+          orElse: () => [],
+        );
     return Scaffold(
       appBar: WhiteAppBar(
         title: DateTime.now().toYYYYMMDD(withJapaneseWeekDay: false),
+        automaticallyImplyLeading: true,
       ),
       body: Padding(
         padding: Measure.p_a16,
@@ -22,86 +44,140 @@ class ResultPage extends StatelessWidget {
             Measure.g_16,
             Row(
               // 自分/自チーム
-              children: const [
-                Icon(Icons.circle, size: 20, color: AppColors.accent),
+              children: [
+                const Icon(Icons.circle, size: 20, color: AppColors.accent),
                 Measure.g_16,
                 Text(
-                  'Thomas Rouxel',
-                  style: TextStyle(
-                    fontSize: 30,
+                  appUserName ?? '',
+                  maxLines: 1,
+                  style: TextStyles.h3().copyWith(
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
               ],
             ),
+            if (result.type == 'doubles')
+              Column(
+                children: [
+                  Measure.g_8,
+                  Row(
+                    // 自分/自チーム
+                    children: [
+                      const Icon(
+                        Icons.circle,
+                        size: 20,
+                        color: AppColors.accent,
+                      ),
+                      Measure.g_16,
+                      Text(
+                        members.isNotEmpty
+                            ? members
+                                .firstWhere(
+                                  (element) =>
+                                      element.memberId == result.partner,
+                                )
+                                .memberName
+                            : '',
+                        maxLines: 1,
+                        style: TextStyles.h3().copyWith(
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             Measure.g_16,
-            Center(
-              // TODO(kokorinosoba): うまくセンタリングできない
-              child: Row(
-                children: [
-                  ScoreText(19),
-                  ScoreText(
-                    21,
-                    bold: true,
-                  ),
-                  ScoreText(19),
-                ],
-              ),
-            ),
-            Measure.g_8,
-            Center(
-              // TODO(kokorinosoba): うまくセンタリングできない
-              child: Row(
-                children: [
-                  ScoreText(
-                    21,
-                    bold: true,
-                  ),
-                  ScoreText(19),
-                  ScoreText(
-                    21,
-                    bold: true,
-                  ),
-                ],
-              ),
+            _PointsScore(
+              result: result,
             ),
             Measure.g_16,
             Row(
               // 対戦相手/チーム
-              children: const [
-                Icon(Icons.circle, size: 20, color: AppColors.accent),
+              children: [
+                const Icon(Icons.circle, size: 20, color: AppColors.accent),
                 Measure.g_16,
                 Text(
-                  'Jonatan Christie',
-                  style: TextStyle(
-                    fontSize: 30,
+                  members.isNotEmpty
+                      ? members
+                          .firstWhere(
+                            (element) =>
+                                element.memberId == result.opponents[0],
+                          )
+                          .memberName
+                      : '',
+                  maxLines: 1,
+                  style: TextStyles.h3().copyWith(
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
               ],
             ),
+            if (result.type == 'doubles')
+              Column(
+                children: [
+                  Measure.g_8,
+                  Row(
+                    // 対戦相手/チーム
+                    children: [
+                      const Icon(
+                        Icons.circle,
+                        size: 20,
+                        color: AppColors.accent,
+                      ),
+                      Measure.g_16,
+                      Flexible(
+                        child: Text(
+                          members.isNotEmpty
+                              ? members
+                                  .firstWhere(
+                                    (element) =>
+                                        element.memberId == result.opponents[1],
+                                  )
+                                  .memberName
+                              : '',
+                          maxLines: 1,
+                          style: TextStyles.h3().copyWith(
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             Measure.g_32,
             Row(
               children: [
-                const Text(
+                Text(
                   'Comment',
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyles.h2(),
                 ),
-                Measure.g_16,
-                IconButton(
-                  onPressed: () {},
-                  icon: const CircleAvatar(
-                    foregroundColor: AppColors.baseWhite,
-                    backgroundColor: AppColors.secondary,
-                    child: FaIcon(Icons.edit, size: 22),
+                Measure.g_8,
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: const BoxDecoration(
+                    color: AppColors.secondary,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      Icons.edit,
+                      size: 18,
+                      color: AppColors.baseWhite,
+                    ),
                   ),
                 ),
               ],
             ),
             Measure.g_16,
-            const Text(
-                '世界バドミントン連盟、略称BWF（英: Badminton World Federation）は、バドミントンの国際競技連盟。1934年に国際バドミントン連盟（IBF、International Badminton Federation）として設立。2006年9月に当名称に改称された。'),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                result.comment.isNotEmpty ? result.comment : 'コメントを書いておこう！',
+              ),
+            ),
           ],
         ),
       ),
@@ -109,11 +185,11 @@ class ResultPage extends StatelessWidget {
   }
 }
 
-class ScoreText extends StatelessWidget {
-  const ScoreText(this.score, {super.key, this.bold});
+class _ScoreText extends StatelessWidget {
+  const _ScoreText(this.score, {this.isBold});
 
   final int score;
-  final bool? bold;
+  final bool? isBold;
 
   @override
   Widget build(BuildContext context) {
@@ -125,10 +201,62 @@ class ScoreText extends StatelessWidget {
           score.toString(),
           style: TextStyle(
             fontSize: 30,
-            fontWeight: bold ?? false ? FontWeight.bold : FontWeight.normal,
+            fontWeight: isBold ?? false ? FontWeight.bold : FontWeight.normal,
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PointsScore extends StatelessWidget {
+  const _PointsScore({required this.result});
+
+  final Result result;
+  @override
+  Column build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _ScoreText(
+              result.yourScore[0],
+              isBold: result.yourScore[0] > result.opponentsScore[0],
+            ),
+            if (result.yourScore.length >= 2)
+              _ScoreText(
+                result.yourScore[1],
+                isBold: result.yourScore[1] > result.opponentsScore[1],
+              ),
+            if (result.yourScore.length == 3)
+              _ScoreText(
+                result.yourScore[2],
+                isBold: result.yourScore[2] > result.opponentsScore[2],
+              ),
+          ],
+        ),
+        Measure.g_8,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _ScoreText(
+              result.opponentsScore[0],
+              isBold: result.opponentsScore[0] > result.yourScore[0],
+            ),
+            if (result.opponentsScore.length >= 2)
+              _ScoreText(
+                result.opponentsScore[1],
+                isBold: result.opponentsScore[1] > result.yourScore[1],
+              ),
+            if (result.opponentsScore.length == 3)
+              _ScoreText(
+                result.opponentsScore[2],
+                isBold: result.opponentsScore[2] > result.yourScore[2],
+              ),
+          ],
+        ),
+      ],
     );
   }
 }
