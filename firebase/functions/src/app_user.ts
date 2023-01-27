0e87b862857b92629b569402f4cd1fb881321303
ryotaewamoto <lengtaiyanben@gmail.com>
2023-01-28 01:14:44 +0900

    feat: fix functions collection name
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- firebase/functions/src/app_user.ts
+++ firebase/functions/src/app_user.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions'
 
 export const onCreateDeleteUser = functions
     .region(`asia-northeast1`)
-    .firestore.document(`deleted_users/{docId}`)
+    .firestore.document(`deletedUsers/{docId}`)
     .onCreate(async (snapshot) => {
         const deleteDocument = snapshot.data()
         const uid = deleteDocument.uid
